aefa648e9229fad19376074f4bbe8ad0a28f4798
VictorWangwz <wangzhen5161@outlook.com>
2019-05-23 17:48:22 -0700

    oa2

--- src/oa2preparation0519/KDistinctCharacters.java
+++ src/oa2preparation0519/KDistinctCharacters.java
@@ -4,8 +4,31 @@ import java.util.*;
 
 public class KDistinctCharacters {
     // Count in string
-    //Count number of substrings with exactly k distinct characters
+    //Count number of substrings with exactly k distinct characters (unique substr count)
     public static int countkDist(String str, int k){
+        Set<String> set = new HashSet<>();
+        char[] char_list = str.toLowerCase().toCharArray();
+        for(int i = 0; i < str.length(); i++){
+            int[] counter = new int[26];
+            int dist = 0;
+            for(int j = i; j < str.length(); j++){
+                if(counter[char_list[j] - 'a'] == 0) {
+                    dist++;
+                }
+                counter[char_list[j] - 'a'] ++;
+                if(dist == k) {
+                    set.add(str.substring(i, j + 1));
+                }else if(dist > k) {
+                    break;
+                }
+            }
+        }
+        return set.size();
+    }
+
+    // Count in string
+    //Count number of substrings with exactly k distinct characters (non-unique substr count)
+    public static int countkDist1(String str, int k){
         int rst = 0;
         char[] char_list = str.toLowerCase().toCharArray();
         for(int i = 0; i < str.length(); i++){
@@ -27,8 +50,27 @@ public class KDistinctCharacters {
     }
 
     // Count in string
-    //Count number of k-substrings with exactly k distinct characters
+    //Count number of k-substrings with exactly k distinct characters (unique substr count count)
     public static int countkDist2(String str, int k){
+        Set<String> rst = new HashSet<>();
+        char[] char_list = str.toCharArray();
+        for(int i = 0; i + k <= char_list.length; i++){
+            String sub_str = str.substring(i, i + k);
+            char[] sub_char_list = sub_str.toCharArray();
+            Set<Character> set = new HashSet<>();
+            for(char c: sub_char_list){
+                set.add(Character.valueOf(c));
+            }
+            if(set.size() == k){
+                rst.add(sub_str);
+            }
+        }
+        return rst.size();
+    }
+
+    // Count in string
+    //Count number of k-substrings with exactly k distinct characters (non-unique substr count count)
+    public static int countkDist21(String str, int k){
         int rst = 0;
         char[] char_list = str.toCharArray();
         for(int i = 0; i + k <= char_list.length; i++){
@@ -39,15 +81,40 @@ public class KDistinctCharacters {
                 set.add(Character.valueOf(c));
             }
             if(set.size() == k){
-                rst ++;
+                rst++;
             }
         }
         return rst;
     }
 
     // Count in string
-    //Count number of k-substrings with exactly k-1 distinct characters
+    //Count number of k-substrings with exactly k-1 distinct characters (unique substr count)
     public static List<String> countkDist3(String str, int k){
+//        int rst = 0;
+        Set<String> rst_set = new HashSet<>();
+        List<String> rst = new ArrayList<>();
+        char[] char_list = str.toCharArray();
+        for(int i = 0; i + k <= char_list.length; i++){
+            String sub_str = str.substring(i, i + k);
+            char[] sub_char_list = sub_str.toCharArray();
+            Set<Character> set = new HashSet<>();
+            for(char c: sub_char_list){
+                set.add(Character.valueOf(c));
+            }
+            if(set.size() == k - 1){
+                rst_set.add(sub_str);
+            }
+        }
+        Iterator<String> itr = rst_set.iterator();
+        while(itr.hasNext()){
+           rst.add(itr.next());
+       }
+        return rst;
+    }
+
+    // Count in string
+    //Count number of k-substrings with exactly k-1 distinct characters (non-unique)
+    public static List<String> countkDist31(String str, int k){
 //        int rst = 0;
         List<String> rst = new ArrayList<>();
         char[] char_list = str.toCharArray();
@@ -90,10 +157,10 @@ public class KDistinctCharacters {
     }
 
     public static void main(String[] args) {
-        String s = "";
-        int k = 3;
-//        int rst = countkDist(s, k);
-        List<String> rst = countkDist3(s, k);
+        String s = "pqpqs";
+        int k = 2;
+        int rst = countkDist1(s, k);
+//        List<String> rst = countkDist1(s, k);
         System.out.println(rst);
 //        char[] chars = {'a', 'd', 'f', 'g', 'k' ,'g'};
 //        int k = 4;
--- src/oa2preparation0519/KNearestPoints.java
+++ src/oa2preparation0519/KNearestPoints.java
@@ -1,9 +1,6 @@
 package oa2preparation0519;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class KNearestPoints {
     public static class Point {
@@ -70,17 +67,74 @@ public class KNearestPoints {
         return rst;
     }
 
+    // Sort and threshold
+    public static List<List<Integer>> nearestRestaurant(List<List<Integer>> restaurant, int n) {
+        int num = restaurant.size();
+        int[] dists = new int[num];
+        for(int i = 0; i < num; i++){
+            dists[i] = calculateDist(restaurant.get(i).get(0),restaurant.get(i).get(1));
+        }
+        Arrays.sort(dists);
+        int dist_limit = dists[n - 1];
+        List<List<Integer>> rst = new ArrayList<>();
+        for(int i = 0; i < restaurant.size(); i++){
+            List<Integer> rest = restaurant.get(i);
+            if(rst.size() >= n){
+                break;
+            }
+            if(calculateDist(rest.get(0), rest.get(1)) <= dist_limit){
+                rst.add(rest);
+            }
+        }
+        return rst;
+    }
+
+    //min-heap
+    public static List<List<Integer>> nearestRestaurant2(List<List<Integer>> restaurant, int n) {
+        List<List<Integer>> rst = new ArrayList<>();
+        PriorityQueue<List<Integer>> pq = new PriorityQueue<>(new Comparator<List<Integer>>() {
+            @Override
+            public int compare(List<Integer> o1, List<Integer> o2) {
+                return calculateDist(o2.get(0), o2.get(1)) - calculateDist(o1.get(0), o1.get(1));
+            }
+        });
+        for(List<Integer> rest: restaurant){
+            pq.offer(rest);
+            while(pq.size() > n){
+                pq.poll();
+            }
+        }
+        while(!pq.isEmpty()){
+            rst.add(pq.poll());
+        }
+        return rst;
+
+    }
+
+    public static int calculateDist(int x, int y){
+        return x * x + y * y;
+    }
+
     public static void main(String[] args) {
         int k = 1;
         Point origin = new Point(3, 1);
-        int[][] points = {{ 0,0},{0,9}};
-        Point[] p = new Point[2];
-        int i = 0;
-        for(; i < p.length; i++){
-            p[i] = new Point(points[i][0], points[i][1]);
+        int[][] points = {{1,3},{-2,2}};
+        List<List<Integer>> restaurant = new ArrayList<>();
+        for(int[] point: points){
+            List<Integer> rest = new ArrayList<>();
+            rest.add(point[0]);
+            rest.add(point[1]);
+            restaurant.add(rest);
         }
-        Point[] rst = kClosest(p, origin, 1);
+        List<List<Integer>> rst = nearestRestaurant2(restaurant, k);
         System.out.println(rst);
+//        Point[] p = new Point[2];
+//        int i = 0;
+//        for(; i < p.length; i++){
+//            p[i] = new Point(points[i][0], points[i][1]);
+//        }
+//        Point[] rst = kClosest(p, origin, 1);
+//        System.out.println(rst);
     }
 
 }
